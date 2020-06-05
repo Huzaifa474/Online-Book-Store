bb0ee71058a6fea8965b30c031d02426689dad93
Huzaifa <Asifsyed860@gmail.com>
2020-06-06 02:07:53 +0500

    HTML file

---
+++ app.js
@@ -0,0 +1,9 @@
+function display()
+{
+    alert("Membershp applied we'll contact you through email in few days");
+    alert("Have a nice day!");
+}
+function message()
+{
+    alert("Apply for mmeberhip on this page");
+}
